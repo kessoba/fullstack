a968ab2677b27dab878ca59b589598514a0a1b95
kessoba <mariamabahdiallo1@gmail.com>
2024-01-26 08:16:57 +0000

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express();
+const APP = express();
 const PORT = process.env.PORT || 5000 ;
 const cors = require("cors");
 const AuthRoutes = require("./Route/AuthRoutes");
@@ -7,15 +7,15 @@ const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 
 
-app.use(cors());
+APP.use(cors());
 // Middleware to parse JSON and urlencoded data
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+APP.use(express.json());
+APP.use(express.urlencoded({ extended: false }));
 
 // Enable CORS
 // Route for authentication
-app.use("/api",AuthRoutes );
+APP.use("/api",AuthRoutes );
 
 // Start the server
-app.listen(PORT, () => console.log("Le serveur a démarré au port " + PORT));
+APP.listen(PORT, () => console.log("Le serveur a démarré au port " + PORT));
 connectDB();
